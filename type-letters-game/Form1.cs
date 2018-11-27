a998143dbf64ef1a9e4a67a4a1b985224dda7202
kpilszak <klaudia.pilszak@gmail.com>
2018-11-27 21:55:56 +0100

    Add class with player's stats

--- type-letters-game/Form1.cs
+++ type-letters-game/Form1.cs
@@ -28,4 +28,28 @@ namespace type_letters_game
             }
         }
     }
+
+    class Stats
+    {
+        public int Total = 0;
+        public int Missed = 0;
+        public int Correct = 0;
+        public int Accuracy = 0;
+
+        public void Update(bool correctKey)
+        {
+            Total++;
+
+            if(!correctKey)
+            {
+                Missed++;
+            }
+            else
+            {
+                Correct++;
+            }
+
+            Accuracy = 100 * Correct / Total;
+        }
+    }
 }
